5c302873cc12e0af1e35748e0849b0824f5ae602
VighneshBilgi <vhbilgi@gmail.com>
2022-07-03 19:38:21 +0530

    Added: UC2_Roll_Die

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -6,6 +6,12 @@ public class SnakeAndLadder {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game:");
+
+        System.out.println();
+        System.out.println("Rolling the die...");
+        int die = (int)(Math.random() * 6)+1;
+        System.out.println("Die number is :"+die);
+
     }
 
 }
