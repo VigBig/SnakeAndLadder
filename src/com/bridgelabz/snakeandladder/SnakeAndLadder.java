05f78ce07a3373cf7ccd837c9035b6d021e4eb46
VighneshBilgi <vhbilgi@gmail.com>
2022-07-03 20:06:35 +0530

    Added: UC3_Options

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,6 +3,9 @@ package com.bridgelabz.snakeandladder;
 public class SnakeAndLadder {
 
     static int position = 0;
+    static final int NO_PLAY = 0;
+    static final int LADDER = 1;
+    static final int SNAKE = 2;
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game:");
@@ -12,6 +15,22 @@ public class SnakeAndLadder {
         int die = (int)(Math.random() * 6)+1;
         System.out.println("Die number is :"+die);
 
+        int option = (int)(Math.random() * 3);
+
+        switch(option){
+            case NO_PLAY:
+                System.out.println("Player remains in same position :"+position);
+                break;
+            case LADDER:
+                position =position + die;
+                System.out.println("Ladder ! Player moves forward "+die+" places. Player is now in position :"+position);
+                break;
+            case SNAKE:
+                position =position - die;
+                System.out.println("Snake ! Player moves backward "+die+" places. Player is now in position :"+position);
+                break;
+        }
+        System.out.println();
     }
 
 }
