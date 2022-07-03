57b53abf027592d186509513a4840456248a9236
VighneshBilgi <vhbilgi@gmail.com>
2022-07-03 21:30:58 +0530

    Added: UC4_Play_Till_Position_100

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,34 +3,56 @@ package com.bridgelabz.snakeandladder;
 public class SnakeAndLadder {
 
     static int position = 0;
+    static int die =0;
+
     static final int NO_PLAY = 0;
     static final int LADDER = 1;
     static final int SNAKE = 2;
 
+    static final int MIN_POS = 0;
+    static final int MAX_POS = 100;
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game:");
-
         System.out.println();
-        System.out.println("Rolling the die...");
-        int die = (int)(Math.random() * 6)+1;
-        System.out.println("Die number is :"+die);
 
-        int option = (int)(Math.random() * 3);
 
-        switch(option){
-            case NO_PLAY:
-                System.out.println("Player remains in same position :"+position);
-                break;
-            case LADDER:
-                position =position + die;
-                System.out.println("Ladder ! Player moves forward "+die+" places. Player is now in position :"+position);
-                break;
-            case SNAKE:
-                position =position - die;
-                System.out.println("Snake ! Player moves backward "+die+" places. Player is now in position :"+position);
-                break;
+        while(position <=MAX_POS){
+            int option = (int)(Math.random() * 3);
+
+            System.out.println("Rolling the die...");
+            die = (int)(Math.random() * 6)+1;
+            System.out.println("Die number is :"+die);
+
+            switch(option){
+                case NO_PLAY:
+
+                    System.out.println("Player remains in same position :"+position);
+                    break;
+
+                case LADDER:
+                    position =position + die;
+                    System.out.println("Ladder ! Player moves forward "+die+" places. Player is now in position :"+position);
+
+                    break;
+
+                case SNAKE:
+
+                    if (position <MIN_POS){
+                        System.out.println("Player remains in same position :"+position);
+                    }else{
+                        position =position - die;
+                        System.out.println("Snake ! Player moves backward "+die+" places. Player is now in position :"+position);
+                    }
+
+                    break;
+            }
+            System.out.println();
         }
-        System.out.println();
+
+
     }
+
+
 
 }
